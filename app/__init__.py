33abd8bacc6bfec4bcf5325b714ed90f776609c6
Neel Patel <send2neelpatel@gmail.com>
2024-03-15 04:02:23 -0400

    Added repl input method with menu/exit/hello

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
+import importlib #For plugin import
+import pkgutil # for plugin import
 import os, sys
 
 import logging, logging.config
 
 from dotenv import load_dotenv
+
+from commands import CommandHandler, Command
 
 # Define the ColoredFormatter class outside the App class
 class ColoredFormatter(logging.Formatter):
@@ -49,6 +53,9 @@ class App:
 
         #Logging
         self.setup_logging()
+
+        #program
+        self.command_handler = CommandHandler()
     
 
     def setup_env_vars(self):
@@ -70,10 +77,61 @@ class App:
         #         if isinstance(handler, logging.StreamHandler):
         #             handler.setFormatter(ColoredFormatter(env_settings=self.env_settings))
 
-
         logging.error("Logging configured.")
 
+    def fetch_plugins(self): 
+        plugins_namespace = 'plugins'
+        path_rel_plugins_folder = 'plugins/'
+        path_abs_plugins_folder = os.path.abspath(path_rel_plugins_folder)
+        #For each item, item's name, and pkgFlag in path's list...
+        print(path_rel_plugins_folder)
+        
+        # Ensure the absolute path of the plugins directory is in the Python path
+        if path_abs_plugins_folder not in sys.path:
+            sys.path.append(path_abs_plugins_folder)
 
+        # Iterate over all packages in the plugins directory
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([path_abs_plugins_folder]):
+            if is_pkg:  # Ensure it's a package
+                try:
+                    # Import the plugin package
+                    plugin_module = importlib.import_module(plugin_name)
+                    # Assume register_plugins is a method to handle the plugin
+                    self.register_plugins(plugin_module, plugin_name)
+                except ImportError as e:
+                    logging.error(f"Error while importing plugin {plugin_name}: {e}")
+    
+    def register_plugins(self, plugin_module, plugin_name):
+        # for each item in folder, check if theres a subclass and register it as a command
+        for item_name in dir(plugin_module):
+            item = getattr(plugin_module, item_name)
+            try:
+                if issubclass(item, (Command)):
+                    self.command_handler.register_command(plugin_name, item())
+                    logging.info(f"Plugin Loaded: {plugin_name}")
+            except TypeError:
+                continue  # Ignore if not class
+
+    def execute_command_in_process(self, command_input):
+            """Method to execute a command in a separate process."""
+            if command_input == "exit":
+                self.exit_event.set()
+            else:
+                self.command_handler.execute_command(command_input)
+
+    
     def start(self): 
-        print("Hello, world");
-        pass
+        #Load Plugins
+            #plugins include menu, exit, hello
+        self.fetch_plugins()
+        #Start repl menu
+
+        try: 
+            while True:  # Check the exit event
+                command_input = input(">>> ").strip()
+                self.command_handler.execute_command(command_input)
+        except KeyboardInterrupt: 
+            logging.info("App interrupted via keyboard. Exiting gracefully.")
+            sys.exit(0)
+        finally: 
+            logging.info("App terminated.")
---
+++ plugins/hello/__init__.py
@@ -0,0 +1,5 @@
+from commands import Command
+class HelloCommand(Command):
+
+    def execute(self):
+        print ("Hello, World!")
