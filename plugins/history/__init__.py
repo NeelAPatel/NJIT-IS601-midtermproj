49ba3fa6511607741f9ac091f4247b6901276720
Neel Patel <send2neelpatel@gmail.com>
2024-03-25 01:59:27 -0400

    added proper logging to history plugin

--- plugins/history/__init__.py
+++ plugins/history/__init__.py
@@ -12,6 +12,7 @@ class HistoryCommand(Command):
         if len(args) == 0: 
             self.default_response()
         else: 
+            # Check which subcommand and fetch that method to trigger
             switchcase_dict = {
                 'show': self.show, 
                 'clear': self.clear, 
@@ -24,8 +25,7 @@ class HistoryCommand(Command):
             }
             method = switchcase_dict.get(args[0], self.default_response)
             method(*args)
-
-        # return method()
+    
     def default_response(self, *args):
         print()
         print('The history plugin used for interacting with the calculations stored in memory.')
@@ -46,24 +46,27 @@ class HistoryCommand(Command):
         
     
     def show(self, *args): 
+        # shows dataframe output
         try:
             print(data_store.hist_df)
         except Exception as e:
             print(f"Error showing history: {e}")
-        # complete
 
-    def clear(self, *args):
-        try:
-            log.info("History - Clearing dataframe to empty table")
-            data_store.hist_df = data_store.hist_df[0:0]  # Clearing the DataFrame
-            log.info("History - Dataframe cleared")
-            print("Cleared history dataframe!")
-            
-            
-            self.save()  # Save after clearing
-        except Exception as e:
-            log.error(f"History - Error clearing history: {e}")
-        #complete
+    def dummy(self, *args): 
+
+        new_row = {'operand': '*', 'num1': 5, 'num2': 4, 'result': 20}
+        data_store.hist_df.loc[len(data_store.hist_df)] = new_row
+
+        print("New Dummy calculation added: ")
+        print(new_row)
+        # print()
+        # print("Result table: ")
+        # print(data_store.hist_df)
+
+        #Autosave
+        self.save()
+        print()
+
 
     def last(self, *args): 
         try:
@@ -73,6 +76,8 @@ class HistoryCommand(Command):
     
 
     def add(self, *args): 
+        log.debug(args)
+        
         # used by calc to save new data with args[0] = null
         try: 
             new_row = {'operand': args[2], 'num1': args[1] , 'num2': args[3], 'result': args[4]}
@@ -81,35 +86,15 @@ class HistoryCommand(Command):
             # print(new_row)
             # print("Result table: ")
             # print(data_store.hist_df)
+            
+            #Autosave
             self.save()
             print()
         except IndexError: 
             log.error("Error: Incorrect arguments for 'history add': $> history add <num1> <sign> <num2> <result>")
         
-
-
-
-
-    def dummy(self, *args): 
-        log.info("")
-        new_row = {'operand': '*', 'num1': 5, 'num2': 4, 'result': 20}
-        data_store.hist_df.loc[len(data_store.hist_df)] = new_row
-
-        print("added: ")
-        print(new_row)
-        print("Result table: ")
-        print(data_store.hist_df)
-        self.save()
-        print()
-
-    # works
-    def save(self, *args):
-        my_df = data_store.hist_df
-        my_df.to_csv(data_store.hist_path, index=False)
-        print("!! Saved to File !!")
-
-
     def delrow (self, *args): 
+        log.debug(args)
 
         if len(args) != 2:
             print("Error: Incorrect number of arguments provided.")
@@ -118,22 +103,32 @@ class HistoryCommand(Command):
             row_index = int(args[1])
             data_store.hist_df = data_store.hist_df.drop(data_store.hist_df.index[row_index])
             data_store.hist_df.reset_index(drop=True, inplace=True)  # Resetting the index and dropping the old index column
+            log.info("Row deleted")
             self.save()
         except ValueError:
             log.error("Error: Provided index is not an integer.")
-        
         except KeyError:
             log.error(f"Error: No row found at index {row_index}.")
         except IndexError: 
             log.error(f"Error: Table is empty, no rows to delete.")
 
 
+    # works
+    def save(self, *args):
+        my_df = data_store.hist_df
+        my_df.to_csv(data_store.hist_path, index=False)
+        print("!! Saved to File !!")
+        log.info("File saved")
+
     def reloadfile(self, *args):
             try:
                 data_store.hist_df = pd.read_csv(data_store.hist_path)
+                log.info("File read successfully")
                 # Resetting the index in case there are gaps from previous operations
                 data_store.hist_df.reset_index(drop=True, inplace=True)
                 print("Data successfully reloaded from file.")
+                log.info("File reloaded")
+
                 self.show()
             except FileNotFoundError:
                 log.error(f"File not found at {data_store.hist_path}. Ensure the file path is correct.")
@@ -142,3 +137,15 @@ class HistoryCommand(Command):
                 data_store.hist_df = pd.DataFrame()  # Creating an empty DataFrame
             except Exception as e:
                 log.error(f"An error occurred while reloading from file: {e}")
+
+    def clear(self, *args):
+        #empties dataframe and saves
+        try:
+            data_store.hist_df = data_store.hist_df[0:0]  # Clearing the DataFrame
+            log.info("History - Dataframe cleared")
+            print("Cleared history dataframe!")
+            
+            #Autosave
+            self.save()
+        except Exception as e:
+            log.error(f"History - Error clearing history: {e}")
