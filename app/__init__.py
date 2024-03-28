da26f04a9b98ff89c1784fe0a6d9f4d65d7a29c6
Neel Patel <send2neelpatel@gmail.com>
2024-03-28 04:48:52 -0400

    Cleaned up app file for basic fixes and comments

--- app/__init__.py
+++ app/__init__.py
@@ -28,32 +28,35 @@ class App:
     
 
     def setup_env_vars(self):
+        # Fetches and returns environment variables to set into app instance var
         return {key: value for key, value in os.environ.items()}
     
     def setup_log(self): 
+        # Sets up the logging system for the entire program
+
         #Set up Loggging path
         os.makedirs('logs', exist_ok=True)
         path_rel_log_conf = 'logging.conf'
         path_abs_log_conf = os.path.abspath(path_rel_log_conf)
-        #Get Log_Level
+        
+        #From ENV - Get Log_Level and its numeric level
         log_level = self.env_settings.get('LOG_LEVEL', 'DEBUG').upper()
-
         numeric_level = getattr(logging, log_level, None)
+
+        #From ENV - Get colorizer flags 
         colorize = self.env_settings.get('LOG_COLORED', 'DEFAULT').upper() in ['COLOR','COLORED','GREY']
     
+        #Setting the logger system - if config file exists, use that, else defaults are provided
         root_logger = logging.getLogger()
-        # Use config file for logger, otherwise take the defaults
         if os.path.exists(path_abs_log_conf):
-            logging.config.fileConfig(path_abs_log_conf, disable_existing_loggers=False)
+            logging.config.fileConfig(path_abs_log_conf, disable_existing_loggers=False) # logger config file
         else:
-            # Fallback basic configuration
-            logging.basicConfig(level=numeric_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            logging.basicConfig(level=numeric_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s') #Default logging config
 
-        # Explicitly set the log level on the root logger and all handlers
+        # Explicitly set the log level on the root logger and all handlers; stream and file andler
         root_logger.setLevel(numeric_level)
         for handler in root_logger.handlers:
             handler.setLevel(numeric_level)
-
             if colorize and isinstance(handler, logging.StreamHandler) and handler.stream == sys.stderr:
                 # Apply ColoredFormatter only to the console handler
                 handler.setFormatter(Colorizer(env_settings=self.env_settings))
@@ -62,23 +65,24 @@ class App:
         log.info("Log sytem configured.")
 
     def fetch_plugins(self): 
+        # Fetches installed plugins from plugins folder 
+
+        #Path to Plugins folder
         path_rel_plugins_folder = 'plugins/'
         path_abs_plugins_folder = os.path.abspath(path_rel_plugins_folder)
-        #For each item, item's name, and pkgFlag in path's list...
         
         # Ensure the absolute path of the plugins directory is in the Python path
         if path_abs_plugins_folder not in sys.path:
             sys.path.append(path_abs_plugins_folder)
-
         log.debug("Plugins ABS Path:"+path_abs_plugins_folder)
 
+        #For each item, item's name, and pkgFlag in path's list...
         # Iterate over all packages in the plugins directory
         for _, plugin_name, is_pkg in pkgutil.iter_modules([path_abs_plugins_folder]):
             if is_pkg:  # Ensure it's a package
                 try:
-                    # Import the plugin package
+                    # Import the plugin package --> register
                     plugin_module = importlib.import_module(plugin_name)
-                    # Assume register_plugins is a method to handle the plugin
                     self.register_plugins(plugin_module, plugin_name)
                 except ImportError as e:
                     log.info(f"Error while importing plugin {plugin_name}: {e}")
@@ -103,24 +107,24 @@ class App:
         data_store.hist_path = path_abs_hist_file
         log.debug("History File ABS Path:"+path_abs_hist_file)
         
-
+        # history file needs to be created and/or retrieved from storage
         columns = ['num1', 'operand', 'num2', 'result']
         if os.path.exists(path_abs_hist_file):
             try:
                 myDF = pd.read_csv(path_abs_hist_file)
     
-                # Check if the file is empty or has no columns
-                if myDF.empty or myDF.columns.empty:
+                # Check if file has no columns
+                if myDF.columns.empty:
                     raise pd.errors.EmptyDataError
     
                 log.info("Loaded existing history file")
             except pd.errors.EmptyDataError:
-                log.info("History File: The file exists but is empty, initializing with columns")
+                log.error("History File: The file exists but is empty, initializing with columns")
                 myDF = pd.DataFrame(columns=columns)
                 myDF.to_csv(path_abs_hist_file, index=False)
                 log.info("History File: Columns added and saved to " + path_abs_hist_file)
         else:
-            log.info("History File: Could not locate pre-existing file")
+            log.error("History File: Could not locate pre-existing file, making new file")
             myDF = pd.DataFrame(columns=columns)
             myDF.to_csv(path_abs_hist_file, index=False)
             log.info("History File: New history file created at " + path_abs_hist_file)
@@ -129,12 +133,14 @@ class App:
         return myDF            
 
     def start(self): 
-        #Load Plugins
-            #plugins include menu, exit, hello
         self.fetch_plugins()
+        log.info("All plugins loaded!")
+
         #Start repl menu
         data_store.hist_df = self.manage_history()
+        log.info("History file ready!")
 
+        print()
         print("Welcome to the Calculator App")
         try: 
             while True:  # Check the exit event
